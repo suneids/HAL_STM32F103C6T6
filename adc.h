fce74b40e433cd4ad11af6fd0d457edc23beec17
suneids <68694104+suneids@users.noreply.github.com>
2026-01-24 01:43:18 +0300

    Clean repository

--- adc.h
+++
@@ -1,9 +0,0 @@
-#ifndef ADC_H
-#define ADC_H
-#define DMA_ENABLE 1
-#define DMA_DISABLE 0
-#include "gpio.h"
-
-void ADCInitMulti(Pin_t *pins, uint16_t count, uint8_t need_dma);
-
-#endif
--- utilities.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTILITIES_H_
-#define UTILITIES_H_
-#include "mcu_config.h"
-#define ONBOARD_LED_PIN 13
-void HAL_assert(uint8_t condition);
-void HAL_assertFailed();
-
-#endif /* UTILITIES_H_ */
